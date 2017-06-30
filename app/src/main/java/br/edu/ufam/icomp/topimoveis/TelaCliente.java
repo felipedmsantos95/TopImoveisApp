c370e374c43bac6b892d170cc5434618dc8cbea2
Felipe Santos <fdms@icomp.ufam.edu.br>
2017-06-30 17:26:02 -0400

    Lista de imoveis concluida

--- app/src/main/java/br/edu/ufam/icomp/topimoveis/TelaCliente.java
+++ app/src/main/java/br/edu/ufam/icomp/topimoveis/TelaCliente.java
@@ -1,17 +1,42 @@
 package br.edu.ufam.icomp.topimoveis;
 
 import android.app.ListActivity;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 
 public class TelaCliente extends ListActivity {
+
+    private ImovelDAO imovelDAO;
+    private SimpleCursorAdapter dados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_cliente);
+
+        this.imovelDAO = new ImovelDAO(this);
+
+        dados = new SimpleCursorAdapter(this,
+                R.layout.imovel_linha,
+                imovelDAO.getImoveis(),
+                new String[] {"endereco", "quartos", "suites", "vagas", "custo"},
+                new int[] {R.id.linhaEndereco,
+                        R.id.linhaQuartos,
+                        R.id.linhaSuites,
+                        R.id.linhaVagas,
+                        R.id.linhaCusto}, 0);
+
+        setListAdapter(dados);
         Log.i("HelloDebug","Atividade TelaCliente iniciada!");
+    }
+
+    public void onListItemClick(ListView l, View v, int pos, long id)
+    {
+
     }
 
     @Override
